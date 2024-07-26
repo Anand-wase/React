9b5ea00f9ae6ee991c3e26592f3f70475e4c166e
SNIPERAnand <anandvardhan.16pav@gmail.com>
2024-07-26 15:58:06 +0530

    Added Header footer sidebar

--- social-media/src/App.jsx
+++ social-media/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import SideBar from './components/SideBar'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='app-Container'>
+    <SideBar/>
+    <div className='content'>
+    <Header/>
+    <Footer/>
+    </div>
+    </div>
+
+
   )
 }
 
