5f501d337575f09fab535581e1007d500b8f2446
SNIPERAnand <anandvardhan.16pav@gmail.com>
2024-07-29 21:44:07 +0530

    UseEffect CleanUp

--- social-media/src/components/PostList.jsx
+++ social-media/src/components/PostList.jsx
@@ -1,20 +1,31 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post"
 import {PostListContext} from '../store/post-list-store'
 import WelcomeMesg from "./WelcomMesg";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const {postlist, addInitialPosts} = useContext(PostListContext);
+  const [fetching, setFetching] = useState(false)
   useEffect(()=>{
-    fetch('https://dummyjson.com/posts')
+    setFetching(true);
+    const controller = new AbortController();
+    const signal = controller.signal;
+    fetch('https://dummyjson.com/posts',{signal})
     .then(res => res.json())
     .then(data =>{
       addInitialPosts(data.posts);
+      setFetching(false);
   });
+  return () => {
+    console.log('Cleaning up UseEffect.');
+    controller.abort();
+  }
   },[])
 
   return<>
-  {postlist.length==0 ? <WelcomeMesg/> : postlist.map((post)=><Post key={post.id} post={post}/>)}
+  {fetching && <LoadingSpinner/>}
+  {!fetching && postlist.length==0 ? <WelcomeMesg/> : postlist.map((post)=><Post key={post.id} post={post}/>)}
   </>
 };
 export default PostList;
