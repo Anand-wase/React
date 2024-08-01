89ef751b7c15692212f926e47654ea37d8a6b71e
SNIPERAnand <anandvardhan.16pav@gmail.com>
2024-08-01 23:52:26 +0530

    Redux Basic Idea

--- Redux/Counter-React-Redux/src/components/Controls.jsx
+++ Redux/Counter-React-Redux/src/components/Controls.jsx
@@ -28,6 +28,11 @@ const Controls = () => {
     });
     inputElement.current.value = "";
   };
+  const handlePrivacyToggle = () => {
+    dispatch({
+      type: 'PRIVACY_TOGGLE'
+    })
+  };
 
   return (
     <>
@@ -37,6 +42,9 @@ const Controls = () => {
       </button>
       <button type="button" className="btn btn-success" onClick={handleDecreament}>
         -1
+      </button>
+      <button type="button" className="btn btn-warning" onClick={handlePrivacyToggle}>
+      Privacy Toggle
       </button>
     </div>
     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
@@ -49,7 +57,6 @@ const Controls = () => {
       </button>
     </div>
     </>
-
   );
 };
 export default Controls;
--- Redux/Counter-React-Redux/src/components/DisplayCounter.jsx
+++ Redux/Counter-React-Redux/src/components/DisplayCounter.jsx
@@ -2,6 +2,6 @@ import { useSelector } from "react-redux";
 
 const DisplayCounter = () => {
   const counter = useSelector(store => store.counter);
-  return <p className="lead mb-4">Counter current value: {counter}</p>
+  return <>{<p className="lead mb-4">Counter current value: {counter}</p>}</>
 };
 export default DisplayCounter;
